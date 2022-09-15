8d61c1647ebf010904076ca82945d944e3056bde
Muhibur Rahman <81737269+codingwithmuhib@users.noreply.github.com>
2022-09-15 09:55:28 +0600

    maltimart ecommerce startup

---
+++ src/assets/data/serviceData.js
@@ -0,0 +1,28 @@
+const serviceData = [
+  {
+    icon: "ri-truck-line",
+    title: "Free Shipping",
+    subtitle: "Lorem ipsum dolor sit amet.",
+    bg: "#fdefe6",
+  },
+  {
+    icon: "ri-refresh-line",
+    title: "Easy Returns",
+    subtitle: "Lorem ipsum dolor sit amet.",
+    bg: "#ceebe9",
+  },
+  {
+    icon: "ri-secure-payment-line",
+    title: "Secure Payment",
+    subtitle: "Lorem ipsum dolor sit amet.",
+    bg: "#e2f2b2",
+  },
+  {
+    icon: "ri-exchange-dollar-line",
+    title: " Back Guarantee",
+    subtitle: "Lorem ipsum dolor sit amet.",
+    bg: "#d6e5fb",
+  },
+];
+
+export default serviceData;
