87bf6ea65398d999f61e5c1a0e28f264548f9296
mesharetivan <146832484+KGmesharetivan@users.noreply.github.com>
2024-01-12 03:31:15 +0800

    Update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+
 import Helmet from "../Helmet/Helmet";
+import "../styles/home.css";
 
 import { Container, Row, Col } from "reactstrap";
 import heroImg from "../assets/images/hero-img.png";
+
+import Services from "../services/Services";
 
 const Home = () => {
   const year = new Date().getFullYear();
@@ -22,7 +28,9 @@ const Home = () => {
                   Unde ad odio eveniet quibusdam aliquam ipsa expedita libero,
                   ullam sapiente animi sunt!
                 </p>
-                <button className="buy__btn">SHOP NOW</button>
+                <motion.button whileTap={{ scale: 1.2 }} className="buy__btn">
+                  <Link to={"/shop"}>SHOP NOW</Link>
+                </motion.button>
               </div>
             </Col>
 
@@ -34,6 +42,8 @@ const Home = () => {
           </Row>
         </Container>
       </section>
+
+      <Services />
     </Helmet>
   );
 };
